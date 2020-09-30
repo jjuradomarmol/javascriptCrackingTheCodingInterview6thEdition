f5d83b663eb4b598247fdea57a3cb0675a506ee8
jjuradomarmol <javierjm88@gmail.com>
2020-09-30 20:48:21 +0200

    Prettier

--- chapter01/01-isUnique.js
+++ chapter01/01-isUnique.js
@@ -1,9 +1,8 @@
-function everyCharUnique (theString) {
-  
-  return !(hasDuplicates = (/([a-zA-Z]).*?\1/).test(theString));
-  
-};
+function everyCharUnique(theString) {
+  return !(hasDuplicates = /([a-zA-Z]).*?\1/.test(theString));
+}
 
-console.log(everyCharUnique('abcd'), 'true');
-console.log(everyCharUnique('mdjq'), 'true');
-console.log(everyCharUnique('abccd'), 'false');
+console.log(everyCharUnique("abcd"), "true");
+console.log(everyCharUnique("mdjq"), "true");
+console.log(everyCharUnique("abccd"), "false");
+

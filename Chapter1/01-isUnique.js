3b946cc3b5cc0e28f45df924f23acdd5c49d5cab
jjuradomarmol <javierjm88@gmail.com>
2020-09-30 19:01:59 +0200

    Create 01-isUnique.js

---
+++ Chapter1/01-isUnique.js
@@ -0,0 +1,9 @@
+function everyCharUnique (theString) {
+  
+  return !(hasDuplicates = (/([a-zA-Z]).*?\1/).test(theString));
+  
+};
+
+console.log(everyCharUnique('abcd'), 'true');
+console.log(everyCharUnique('mdjq'), 'true');
+console.log(everyCharUnique('abccd'), 'false');

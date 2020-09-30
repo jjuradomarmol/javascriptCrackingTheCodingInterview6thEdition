5bfa9fb160f684e7b9642f85b0bee09308315628
jjuradomarmol <javierjm88@gmail.com>
2020-09-30 19:14:50 +0200

    Delete 01-isUnique.js

--- Chapter1/01-isUnique.js
+++
@@ -1,9 +0,0 @@
-function everyCharUnique (theString) {
-  
-  return !(hasDuplicates = (/([a-zA-Z]).*?\1/).test(theString));
-  
-};
-
-console.log(everyCharUnique('abcd'), 'true');
-console.log(everyCharUnique('mdjq'), 'true');
-console.log(everyCharUnique('abccd'), 'false');

94825e9568e40449aeeabb82a5bfc7d015d7e431
jjuradomarmol <javierjm88@gmail.com>
2020-10-01 14:52:39 +0200

    Update 01-isUnique.js

--- chapter01/01-isUnique.js
+++ chapter01/01-isUnique.js
@@ -1,6 +1,6 @@
 function everyCharUnique(theString) {
   let hasDuplicates = /([a-zA-Z]).*?\1/.test(theString);
-  return !(hasDuplicates);
+  return !hasDuplicates;
 }
 
 console.log(everyCharUnique("abcd"), "true");

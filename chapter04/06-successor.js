081fdaea64cdfe67279528c744ac1bd849c3dbb7
jjuradomarmol <javierjm88@gmail.com>
2020-11-10 19:56:59 +0100

    Create 06-successor.js

---
+++ chapter04/06-successor.js
@@ -0,0 +1,53 @@
+class BinarySearchTreeParent {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.parent = null;
+  }
+}
+
+function findSuccessor(node) {
+  var successor = null;
+  if (node.right !== null) {
+    successor = node.right;
+    while (successor.left !== null) {
+      successor = successor.left;
+    }
+  } else if (node.parent !== null) {
+    var currentNode = node;
+    while (currentNode.parent !== null && successor === null) {
+      if (currentNode.parent.left === currentNode) {
+        successor = currentNode.parent;
+      }
+      currentNode = currentNode.parent;
+    }
+  }
+  return successor;
+}
+
+// Proofs
+
+var a = new BSTp(10);
+var b = new BSTp(2);
+var c = new BSTp(3);
+var d = new BSTp(4);
+var e = new BSTp(6);
+var f = new BSTp(5);
+var g = new BSTp(7);
+
+a.left = b;
+b.parent = a;
+b.right = c;
+c.parent = b;
+c.right = d;
+d.parent = c;
+d.right = e;
+e.parent = d;
+e.left = f;
+f.parent = e;
+e.right = g;
+g.parent = e;
+
+console.log(findSuccessor(f).value, 6);
+console.log(findSuccessor(g).value, 10);

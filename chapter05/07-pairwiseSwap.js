857eea8137e1988bf33dbcda309d27f24f8ab49c
jjuradomarmol <javierjm88@gmail.com>
2020-11-13 13:24:48 +0100

    Create 07-pairwiseSwap.js

---
+++ chapter05/07-pairwiseSwap.js
@@ -0,0 +1,14 @@
+function pairwiseSwap(number) {
+  let binaryNumber = number.toString(2).split("");
+  for (let i = 0; i <= binaryNumber.length - 2; i += 2) {
+    [binaryNumber[i], binaryNumber[i + 1]] = [
+      binaryNumber[i + 1],
+      binaryNumber[i],
+    ];
+  }
+  return binaryNumber;
+}
+
+// Proofs
+
+console.log(pairwiseSwap(4));

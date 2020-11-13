e0893924abc5240e71b2ceb13aac07fc4ff9bc2b
jjuradomarmol <javierjm88@gmail.com>
2020-11-13 13:01:38 +0100

    Create 06-conversion.js

---
+++ chapter05/06-conversion.js
@@ -0,0 +1,12 @@
+function countBits(number) {
+  var string = number.toString(2);
+  return string.split("").reduce((numBits, char) => {
+    return char === "1" ? numBits + 1 : numBits;
+  }, 0);
+}
+
+function conversion(number1, number2) {
+  return countBits(number1 ^ number2);
+}
+
+console.log(conversion(29, 15), 2);

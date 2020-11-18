91ebb4c4e790c55b475d0a8f95ffb2f3a7e8fabd
jjuradomarmol <javierjm88@gmail.com>
2020-11-18 13:37:58 +0100

    Create 08-permutationWithDuplicates.js

---
+++ chapter08/08-permutationWithDuplicates.js
@@ -0,0 +1,31 @@
+function permuteWithDuplicates(string) {
+  let answers = [];
+  let recurse = function (currentPermutation, remainingChars) {
+    if (remainingChars.length === 0) {
+      answers.push(currentPermutation);
+    } else {
+      let usedChars = [];
+      for (let i = 0; i < remainingChars.length; i++) {
+        if (!usedChars[remainingChars.charAt(i)]) {
+          usedChars[remainingChars.charAt(i)] = true;
+          recurse(
+            currentPermutation + remainingChars.charAt(i),
+            remainingChars.slice(0, i) + remainingChars.slice(i + 1)
+          );
+        }
+      }
+    }
+  };
+  recurse("", string);
+  return answers;
+}
+
+// Proofs
+
+var test1 = "aaa";
+var test2 = "abc";
+var test3 = "aba";
+
+console.log(permuteWithDuplicates(test1));
+console.log(permuteWithDuplicates(test2));
+console.log(permuteWithDuplicates(test3));

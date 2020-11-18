18eed889bdb61f4ac779b37d0c19d5adafa91aed
jjuradomarmol <javierjm88@gmail.com>
2020-11-18 13:27:49 +0100

    Create 07-permutationsWithoutDuplicates.js

---
+++ chapter08/07-permutationsWithoutDuplicates.js
@@ -0,0 +1,22 @@
+function permuteWithoutDuplicates(string) {
+  let answers = [];
+  let recurse = function (currentPermutation, remainingChars) {
+    if (remainingChars.length === 0) {
+      answers.push(currentPermutation);
+    } else {
+      for (let i = 0; i < remainingChars.length; i++) {
+        recurse(
+          currentPermutation + remainingChars.charAt(i),
+          remainingChars.slice(0, i) + remainingChars.slice(i + 1)
+        );
+      }
+    }
+  };
+  recurse("", string);
+  return answers;
+}
+
+// Proofs
+
+var testString = "abcd";
+console.log(permuteWithoutDuplicates(testString));

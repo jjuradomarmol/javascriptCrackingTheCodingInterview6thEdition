458da05b1d0a29db29b4e586461d4f171fcc53e9
jjuradomarmol <javierjm88@gmail.com>
2020-11-18 13:02:56 +0100

    Create 06-towersOfHanoi.js

---
+++ chapter08/06-towersOfHanoi.js
@@ -0,0 +1,62 @@
+class TowersOfHanoi {
+  constructor(n) {
+    this.first = [];
+    this.second = [];
+    this.third = [];
+    for (var i = n; i >= 1; i--) {
+      this.first.push(i);
+    }
+  }
+
+  move(start, mid, destination, depth) {
+    if (depth === 0) {
+      return;
+    } else if (depth === 1) {
+      destination.push(start.pop());
+    } else {
+      var currentDepth = depth;
+      var shortTower;
+      var placePiece;
+      if (depth % 2 === 0) {
+        shortTower = mid;
+        placePiece = destination;
+      } else {
+        shortTower = destination;
+        placePiece = mid;
+      }
+      var shortTowerDepth = 0;
+      shortTower.push(start.pop());
+      shortTowerDepth++;
+      currentDepth--;
+      while (currentDepth > 0) {
+        placePiece.push(start.pop());
+        currentDepth--;
+        this.move(shortTower, start, placePiece, shortTowerDepth);
+        shortTowerDepth++;
+        [shortTower, placePiece] = [placePiece, shortTower];
+      }
+    }
+  }
+}
+
+// Proofs
+
+var towersOfHanoi = new TowersOfHanoi(5);
+console.log(towersOfHanoi);
+towersOfHanoi.move(
+  towersOfHanoi.first,
+  towersOfHanoi.second,
+  towersOfHanoi.third,
+  towersOfHanoi.first.length
+);
+console.log(towersOfHanoi);
+
+var towersOfHanoi2 = new TowersOfHanoi(6);
+console.log(towersOfHanoi2);
+towersOfHanoi.move(
+  towersOfHanoi2.first,
+  towersOfHanoi2.second,
+  towersOfHanoi2.third,
+  towersOfHanoi2.first.length
+);
+console.log(towersOfHanoi2);

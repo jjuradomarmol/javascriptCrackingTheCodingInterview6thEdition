124becdd4ab56137cd1a946989702ac097a79154
jjuradomarmol <javierjm88@gmail.com>
2020-11-19 13:35:09 +0100

    Update 04-sortedSearchNoSize.js

--- chapter10/04-sortedSearchNoSize.js
+++ chapter10/04-sortedSearchNoSize.js
@@ -2,7 +2,7 @@ function sortedSearchNoSize(array, searched) {
   return array.indexOf(searched);
 }
 
-listy = [2, 3, 4, 6, 3];
+let listy = [2, 3, 4, 6, 3];
 console.log(sortedSearchNoSize(listy, 3) === 1);
 console.log(sortedSearchNoSize(listy, 2) === 0);
 console.log(sortedSearchNoSize(listy, 6) === 3);

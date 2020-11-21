a974688485dc1d1176bfd711ed4329bee4e74366
jjuradomarmol <javierjm88@gmail.com>
2020-11-21 16:24:34 +0100

    Update 04-sortedSearchNoSize.js

--- chapter10/04-sortedSearchNoSize.js
+++ chapter10/04-sortedSearchNoSize.js
@@ -1,11 +1,20 @@
-function sortedSearchNoSize(array, searched) {
-  return array.indexOf(searched);
+function sortedSearchNoSize(array = [], searched = 0) {
+  let indexesArray = [],
+    index = undefined;
+
+  while (array.indexOf(searched) >= 0) {
+    index == undefined
+      ? (index = array.indexOf(searched))
+      : (index += array.indexOf(searched) + 1);
+    indexesArray.push(index);
+    array = array.slice(array.indexOf(searched) + 1);
+  }
+
+  return indexesArray;
 }
 
-let listy = [2, 3, 4, 6, 3];
-console.log(sortedSearchNoSize(listy, 3) === 1);
-console.log(sortedSearchNoSize(listy, 2) === 0);
-console.log(sortedSearchNoSize(listy, 6) === 3);
-console.log(sortedSearchNoSize(listy, 1) === -1);
-console.log(sortedSearchNoSize(listy, 10) === -1);
-console.log(sortedSearchNoSize(listy, 5) === -1);
+let listy = [2, 3, 4, 2, 6, 3, 2];
+console.log(sortedSearchNoSize(listy, 3));
+console.log(sortedSearchNoSize(listy, 2));
+console.log(sortedSearchNoSize(listy, 6));
+console.log(sortedSearchNoSize(listy, 10));
